1ecc83442bb073e4fc6d4ca0fffd3de555dc365a
dhruvinddev <dhruvinddev@gmail.com>
2024-02-03 17:29:10 +0530

    - Updated snippets - Added ignore files to NvTree

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -1,7 +1,7 @@
 local options = {
   filters = {
     dotfiles = false,
-    exclude = { vim.fn.stdpath "config" .. "/lua/custom" },
+    exclude = { vim.fn.stdpath "config" .. "/lua/custom" .. ".DS_Store" },
   },
   disable_netrw = true,
   hijack_netrw = true,
